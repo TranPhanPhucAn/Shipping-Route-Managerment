24e65817dcb35df233b189acfe5c6b189a47ceff
ThuHoa0801 <127923718+ThuHoa0801@users.noreply.github.com>
2024-07-24 16:58:42 +0700

    fix .env

--- frontend/src/components/layout/Header.tsx
+++ frontend/src/components/layout/Header.tsx
@@ -2,6 +2,8 @@
 import React from "react";
 import { Menu } from "antd";
 import type { MenuProps } from "antd";
+import Link from "next/link";
+import { useState } from "react";
 import {
   HomeOutlined,
   AppstoreOutlined,
@@ -9,37 +11,37 @@ import {
 } from "@ant-design/icons";
 type MenuItem = Required<MenuProps>["items"][number];
 
-function getItem(
-  label: React.ReactNode,
-  key: React.Key,
-  icon?: React.ReactNode,
-  children?: MenuItem[],
-  type?: "group"
-): MenuItem {
-  return {
-    key,
-    icon,
-    children,
-    label,
-    type,
-  } as MenuItem;
-}
-
 const items: MenuItem[] = [
-  getItem("Homepage", "/", <HomeOutlined />),
-  getItem("Management User", "/users", <UserOutlined />),
-  getItem("Management Route", "/routes", <AppstoreOutlined />),
+  {
+    label: <Link href={"/"}>Home Page</Link>,
+    key: "homepage",
+    icon: <HomeOutlined />,
+  },
+  {
+    label: <Link href={"/users"}>Manage Users</Link>,
+    key: "users",
+    icon: <UserOutlined />,
+  },
+  {
+    label: <Link href={"/routes"}>Manage Routes</Link>,
+    key: "routes",
+    icon: <AppstoreOutlined />,
+    // disabled: true,
+  },
 ];
 const Header: React.FC = () => {
+  const [current, setCurrent] = useState("homepage");
+
   const onClick: MenuProps["onClick"] = (e) => {
     console.log("click ", e);
+    setCurrent(e.key);
   };
 
   return (
     <Menu
       onClick={onClick}
       style={{ width: "100%" }}
-      defaultSelectedKeys={["1"]}
+      selectedKeys={["current"]}
       mode="horizontal"
       theme="dark"
       items={items}
