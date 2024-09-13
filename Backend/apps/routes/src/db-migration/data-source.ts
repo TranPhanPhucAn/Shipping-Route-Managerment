301ee9f98752dc409f818ac695cea7a38c113ab5
TranPhanPhucAn <21120407@student.hcmus.edu.vn>
2024-09-13 18:03:03 +0700

    merge author

--- Backend/apps/routes/src/db-migration/data-source.ts
+++ Backend/apps/routes/src/db-migration/data-source.ts
@@ -1,6 +1,9 @@
 import { DataSource, DataSourceOptions } from 'typeorm';
 import * as dotenv from 'dotenv';
 import { Route } from '../routes/entities/route.entity';
+import { Port } from '../ports/entities/port.entity';
+import { Vessel } from '../vessels/entities/vessel.entity';
+import { Schedule } from '../schedules/entities/schedule.entity';
 dotenv.config();
 
 export const dataSourceOptions: DataSourceOptions = {
@@ -11,7 +14,7 @@ export const dataSourceOptions: DataSourceOptions = {
   password: process.env.POSTGRES_PASSWORD,
   database: process.env.POSTGRES_DB,
   // entities: ['dist/apps/**/**/**/entities/*{.js,.ts}'],
-  entities: [Route],
+  entities: [Route, Port, Vessel, Schedule],
   migrations: ['migrations/**'],
 };
 const dataSource = new DataSource(dataSourceOptions);
