c2dc0904c22bbcaaed0d90ae8a706fa8c639e7b1
TranPhanPhucAn <21120407@student.hcmus.edu.vn>
2024-10-02 14:42:17 +0700

    search

---
+++ Backend/migrations/1727854385332-NewMigrations.ts
@@ -0,0 +1,14 @@
+// import { MigrationInterface, QueryRunner } from "typeorm";
+
+// export class NewMigrations1727854385332 implements MigrationInterface {
+//     name = 'NewMigrations1727854385332'
+
+//     public async up(queryRunner: QueryRunner): Promise<void> {
+//         await queryRunner.query(`ALTER TABLE "ports" RENAME COLUMN "location" TO "country"`);
+//     }
+
+//     public async down(queryRunner: QueryRunner): Promise<void> {
+//         await queryRunner.query(`ALTER TABLE "ports" RENAME COLUMN "country" TO "location"`);
+//     }
+
+// }
