63b08c8c2cbc6e923ffc5ffa0142514d80ffa9f6
ThuHoa0801 <127923718+ThuHoa0801@users.noreply.github.com>
2024-10-17 13:33:35 +0700

    seeding routesservice

--- Backend/apps/routes/src/seeding/seeding.resolver.ts
+++ Backend/apps/routes/src/seeding/seeding.resolver.ts
@@ -1,13 +1,17 @@
 import { Resolver, Mutation } from '@nestjs/graphql';
 import { SeedingService } from './seeding.service';
-import { seedRouteResponse } from '../types/route.types';
+// import { seedRouteResponse } from '../types/route.types';
 
 @Resolver()
 export class SeedingResolver {
   constructor(private readonly seedingService: SeedingService) {}
 
-  @Mutation(() => seedRouteResponse)
-  seedRoute() {
-    return this.seedingService.seed();
+  @Mutation(() => String)
+  async seed(): Promise<string> {
+    try {
+      return await this.seedingService.seed();
+    } catch (error) {
+      throw new Error('Seeding failed');
+    }
   }
 }
